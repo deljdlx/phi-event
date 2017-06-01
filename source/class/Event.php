de586ec00c2237e78f3dc6c20153563e8c782ed7
Vagrant Default User <vagrant@jessie.raw>
2017-06-01 13:53:02 +0000

    synchro

---
+++ source/class/Event.php
@@ -0,0 +1,39 @@
+<?php
+namespace Phi\Event;
+
+
+class Event
+{
+
+    public $name='event';
+    public $data;
+    public $source;
+
+    protected $defaultPrevented=false;
+
+
+    public function __construct($name='event', $data=array(), $source=null) {
+        $this->name=$name;
+        $this->data=$data;
+        $this->source=$source;
+    }
+
+    public function getSource() {
+        return $this->source;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function preventDefault() {
+        $this->defaultPrevented=true;
+    }
+
+    public function isDefaultPrevented() {
+        return $this->defaultPrevented;
+    }
+
+
+
+}
